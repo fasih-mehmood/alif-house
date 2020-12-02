a58ae438a33bc8f9e0616b5ce22d652e3cd898cb
Fasih Mehmood <48308392+fasih-mehmood@users.noreply.github.com>
2020-12-02 18:52:22 +0500

    Added navigation. Added sign in bar. Added swal popup. Fixed some links.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// sweet alert to show error
 function error(message) {
   Swal.fire({
     icon: "error",
@@ -7,56 +8,54 @@ function error(message) {
   });
 }
 
+// function to open sidebars
+function openNav(nav,direction) {
+  $("#"+nav).removeClass("hidden animate__fadeOut"+direction);
+  $("#"+nav).addClass("animate__fadeIn"+direction);
+  $(".sidebar-bg-overlay").removeClass("hidden animate__fadeOut");
+  $(".sidebar-bg-overlay").addClass("animate__fadeIn");
+}
+
+// function to close sidebars
+function closeNav(nav,direction) {
+  $("#"+nav).removeClass("animate__fadeIn"+direction);
+  $("#"+nav).addClass("animate__fadeOut"+direction);
+  $(".sidebar-bg-overlay").addClass("animate__fadeOut");
+  $(".sidebar-bg-overlay").removeClass("animate__fadeIn");
+  setTimeout( function(){ 
+    $(".sidebar-bg-overlay").addClass("hidden");
+  }  , 500 );
+}
+
+// function to display forgot password 
+function showForgotPass() {
+  $('#forgotPassword').addClass("active");
+}
+
+// function to hide forgot password 
+function hideForgotPass() {
+  $('#forgotPassword').removeClass("active");
+}
+
+// function to show arts submenu 
+function showArtsSubMenu() {
+  $('#arts-submenu').addClass("active");
+}
+
+// function to hide arts submenu 
+function hideSubMenu(el) {
+  this.event.stopPropagation();
+  $(el).parent().parent().removeClass("active");
+}
+
 $(document).ready(function () {
-  const reviewForm = document.querySelector("#commentform");
-  const contactForm = document.querySelector("#contactForm");
 
-  contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (contactForm.checkValidity() == false) {
-      error();
-      return;
-    }
-    var contactEmail = contactForm.email.value;
-    Swal.fire({
-      icon: "success",
-      title: "Success",
-      confirmButtonColor: "greenyellow",
-      text: "Your message has been sent successfully!",
-    });
-    contactForm.reset();
-  });
+  artsSubMenu = $("#arts-submenu");
 
-  subscribeNews.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (subscribeNews.checkValidity() == false) {
-      error();
-      return;
-    }
-    var emailSub = subEmail.value;
-    Swal.fire({
-      icon: "success",
-      title: "Success",
-      confirmButtonColor: "greenyellow",
-      text: "You have been subscribed successfully!",
-    });
-    subscribeNews.reset();
-  });
-
-  reviewForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (reviewForm.checkValidity() == false) {
-      error();
-      return;
-    }
-    var emailRev = reviewForm["email"].value;
-    Swal.fire({
-      icon: "success",
-      title: "Success",
-      confirmButtonColor: "greenyellow",
-      text: "Your review has been submitted!",
-    });
-    reviewForm.reset();
+  // close sidebar on clicking outside the container
+  $('.sidebar-bg-overlay').click(e=>{
+    closeNav('accountBar','Right');
+    closeNav('sideNav','Left');
   });
 
   /* Navigation scroll */
@@ -104,6 +103,57 @@ $(document).ready(function () {
       }
     });
 
+  subscribeNews.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (subscribeNews.checkValidity() == false) {
+      error();
+      return;
+    }
+    var emailSub = subEmail.value;
+    Swal.fire({
+      icon: "success",
+      title: "Success",
+      confirmButtonColor: "greenyellow",
+      text: "You have been subscribed successfully!",
+    });
+    subscribeNews.reset();
+  });
+
+  commentform.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (commentform.checkValidity() == false) {
+      error();
+      return;
+    }
+    var emailRev = commentform["email"].value;
+    Swal.fire({
+      icon: "success",
+      title: "Success",
+      confirmButtonColor: "greenyellow",
+      text: "Your review has been submitted!",
+    });
+    commentform.reset();
+  });
+});
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (contactForm.checkValidity() == false) {
+    error();
+    return;
+  }
+  var contactEmail = contactForm.email.value;
+  Swal.fire({
+    icon: "success",
+    title: "Success",
+    confirmButtonColor: "greenyellow",
+    text: "Your message has been sent successfully!",
+  });
+  contactForm.reset();
+});
+
+$("#toggleCanvas").click(e=> {
+  $("#c").toggle("fold",2000);
 });
 
   /* Drawing matrix on canvas */
